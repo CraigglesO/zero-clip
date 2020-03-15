ebac37a3c1d6ac95d17a85bbf00fca9d3b23020d
CraigglesO <oconnorct1@gmail.com>
2020-03-15 08:47:43 -0400

    fixed offset for anchor

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function zeroClip (polygon, offset = 0) {
   // prep & store the anchor
   let indexPos = 1
   let i, rl
+  vertices.push(...polygon[0][0])
 
   // run through every point and create a triangle from the anchor
   for (const ring of polygon) {
